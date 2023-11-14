2678703a539929552babb8f658c7e11835b5426a
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-11-15 00:15:02 +0530

    Update ninja'sTraining.cpp

--- DP/ninja'sTraining.cpp
+++ DP/ninja'sTraining.cpp
@@ -1,1 +1,29 @@
+// RECURSION
 
+
+int f(int day, int last, vector<vector<int>> &task){
+    int maxi;
+    if(day == 0){
+        maxi = 0;
+        for(int t=0; t<3; t++){
+            if(t != last){
+                maxi = max(maxi, task[0][t]);
+            }
+        }
+        return maxi;
+    }
+    
+    maxi = 0;
+
+    for(int t=0; t<3; t++){
+        if (t != last) {
+            maxi = max(maxi, task[day][t] + f(day - 1, t, task));
+        }
+    }
+    return maxi;
+}
+
+int ninjaTraining(int n, vector<vector<int>> &points)
+{
+    return f(n-1, 3, points);
+}
